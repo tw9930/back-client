45361e060d0afbcf28517eb5c719187a27108738
tw <tw9930942@163.com>
2021-10-29 15:16:05 +0800

    小修改

--- src/router/router.js
+++ src/router/router.js
@@ -3,19 +3,19 @@ import VueRouter from "vue-router";
 
 import route from "./route.js";
 Vue.use(VueRouter)
-//全局路由导航守卫
-// router.beforeEach((to,form,netx)=>{
-// if (to.name){
-//     netx()
-// }
-//
-// })
 
 //创建路由器
 const routes  = new VueRouter({
     routes: route
 })
+// 路由守卫导航
+/***
 routes.beforeEach((to,form,next)=>{
 //    to 代表即将去哪里，form：代表当前要离开的路由 next：
+        if(to.path ==='/login') return next() //如果去的是登录页面直接放行
+        let token = window.sessionStorage.getItem('token') //保存token
+        if (token) {return next() } else next('/login') //检查token
+
 })
+ ***/
 export default routes
